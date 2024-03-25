e225bd71a4485fd753777e3811ce073ce4a48703
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-03-25 09:52:08 +0100

    tests using CombinationApprovals.verifyAllCombinations

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,21 +1,21 @@
 package com.gildedrose;
 
-import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
     @Test
     void testUpdateQuality() {
-        Approvals.verify(doUpdateQuality("foo", 0, 0));
+//        Approvals.verify(doUpdateQuality("foo", 0, 0));
+         CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                new String[] { "foo", "Aged Brie" },
+                new Integer[] { 0, 5 },
+                new Integer[] { 0 });
     }
 
-    @Test
-    void testBrie() {
-        Approvals.verify(doUpdateQuality("Aged Brie", 0, 0));
-    }
-
-    private static String doUpdateQuality(String name, int sellIn, int quality) {
+    private String doUpdateQuality(String name, int sellIn, int quality) {
         Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
