d4ffad385301f8cfa895463ab4c732afa3d651df
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-03-25 11:16:56 +0100

    case "Conjured"

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -53,6 +53,21 @@ class GildedRose {
                     item.quality = 0;
                 }
                 break;
+            case "Conjured":
+                if (item.quality > 0) {
+                    item.quality = item.quality - 2;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 2;
+                    }
+                }
+                if (item.quality < 0)
+                    item.quality = 0;
+                break;
             default:
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
