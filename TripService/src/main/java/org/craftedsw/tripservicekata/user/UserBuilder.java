8fc1c2d95f2a447bb2548e7f80323e4dc7a1ca23
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-04-03 16:32:45 +0200

    feat: User builder

--- TripService/src/main/java/org/craftedsw/tripservicekata/user/UserBuilder.java
+++ TripService/src/main/java/org/craftedsw/tripservicekata/user/UserBuilder.java
@@ -11,15 +11,43 @@ public class UserBuilder {
 		return new UserBuilder();
 	}
 
+	private User[] friends = new User[] {};
+
+	// Pattern (manière de faire) élégante
+	public UserBuilder withFriends(User... users) {
+		this.friends = users;
+		return this;
+	}
+
+	private Trip[] trips = new Trip[] {};
+
+	// Pattern (manière de faire) élégante
+	public UserBuilder withTrips(Trip... users) {
+		this.trips = users;
+		return this;
+	}
+
 	public User build() {
 		User user = new User();
+		for (User friend : friends)
+			user.addFriend(friend);
 		return user;
 	}
+
+
 
 //	void WANNADOTHIS {
 //		User u = createUser()
 //				.withFriends(new User(), new User(), new User())
 //				.withTrips(new Trip())
 //				.build();
+//
+//	UserBuilder builder = createUser().withFriends(new User());
+//	user1 = builder.build()
+//	user2 = builder.build()
+//
+//	UserBuilder builder2 = new UservBuilder();
+//	builder2.withFriends(new User)
+//	User u = builder2.build();
 //	}
 }
